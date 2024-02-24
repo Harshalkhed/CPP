095b0ff161ea6dc6fba6c554a0f73a4275573d49
Harshal Khedekar <32055228-hskhedekar@users.noreply.replit.com>
2024-02-24 06:17:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@FirstCppProgram/C++ Introduction@Firstprogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  cout << "Lets make an infy runner game !\n";
+  cout << "Our main charater is a  skate boarder\n";
+  cout << "Looks like he needs some help \n he is stuck in a looop \n";
+  cout << "Need some help with the obstacles says the....\n";
+  cout << "The legendary skate boarder\n";
+  std::cout << sizeof(int);
+}
---
+++ .ccls-cache/@home@runner@FirstCppProgram/main.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+using namespace std;
+
+int main() { std::cout << "Hello Looks like Im back !\n"; }
